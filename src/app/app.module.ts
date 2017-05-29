2ecb0568d1183eeefe893019e58c087ba03f2915
Rodrigo <rodrigo.fcardoso@hotmail.com>
2017-05-29 17:23:03 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {HomeModule} from "./home/home.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/nav/nav.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  selector: 'nav-component',
+  templateUrl: 'nav.component.html',
+  styleUrls: ['nav.component.css']
+})
+export class NavComponent{
+}
